283a1071f11e48075909ceade2a619d21ed85447
DrMike77 <101877786+DrMike77@users.noreply.github.com>
2022-04-20 17:04:23 +0300

    Add files via upload
    Please check

---
+++ Lesson_8_task_2.py
@@ -0,0 +1,20 @@
+class CustomZeroDivisionError(Exception):
+    pass
+def get_numerator() -> int:
+    return int(input("Введите числитель >>> "))
+def get_denominator() -> int:
+    value = int(input("Введите знаменатель >>> "))
+
+    if value == 0:
+        raise CustomZeroDivisionError
+    return value
+while True:
+    try:
+        numerator = get_numerator()
+        denominator = get_denominator()
+
+        print(f"Результат = {numerator / denominator}")
+    except CustomZeroDivisionError:
+        print("Вы ввели 0 в качестве знаменателя, так не пойдет")
+    except KeyboardInterrupt:
+        break
